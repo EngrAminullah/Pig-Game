81d7752c8dc57943655d94445cd148e0658600cc
Engr Aminullah <96494231+EngrAminullah@users.noreply.github.com>
2022-07-25 15:07:40 +0500

    selecting and starting elements

--- final/script.js
+++ final/script.js
@@ -96,12 +96,19 @@
 
 // btnNew.addEventListener('click', init);
 
+// Selecting elements
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
+// const current0El = document.getElementById('current--0');
+// const current1El = document.getElementById('current--1');
 
 const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn--new');
-const btnRoll = document.querySelector('.btn--roll');
-const btnHold = document.querySelector('.btn--hold');
+// const btnNew = document.querySelector('.btn--new');
+// const btnRoll = document.querySelector('.btn--roll');
+// const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('.hidden');
